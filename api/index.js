c347a7eeb06bec112c84e066d6bb5c86a7cc502e
Johnson <johnsonkarasanyi@gmail.com>
2022-09-14 11:42:00 +0200

    add routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+//if you are importing JS file you should also put js extension in express server
+import authRouter from '../api/routes/auth.js'
 const app = express()
 // from now we are able to reach our data
 dotenv.config();
@@ -12,9 +14,7 @@ const connect = async () => {
         throw error;
     }
 }
-app.get("/",(req,res)=>{
-    res.send("Hello First Request")
-})
+// if you want to look your connection in editor
 mongoose.connection.on("disconnected",()=>{
     console.log("MongoDb disconnected!")
 })
