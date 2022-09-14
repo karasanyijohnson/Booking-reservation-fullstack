797a3a9266e559037edef0daa09f77efe4b35bb1
Johnson <johnsonkarasanyi@gmail.com>
2022-09-14 13:58:11 +0200

    creating hotel model and import to its routes

--- api/models/user.js
+++ api/models/user.js
@@ -1,37 +1,55 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const hotelSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    type:{
-        type:String,
-        required:true
+    type: {
+        type: String,
+        required: true
     },
-    city:{
-        type:String,
-        required:true
+    city: {
+        type: String,
+        required: true
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+        type: String,
+        required: true
     },
-    distance:{
-        type:String,
-        required:true
+    distance: {
+        type: String,
+        required: true
     },
-    photos:{
-        type:[string],
+    photos: {
+        type: [string],
     },
-    desc:{
-        type:String,
-        required:true
+    desc: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    rooms: {
+        type: [String],
+
+    },
+    chipestPrice: {
+        type: Number,
+       required:true
     },
     rating:{
         type:Number,
         min:0,
         max:5
     },
+    featured:{
+        type:Boolean,
+        default:false
+    },
 })
+export default mongoose.model("Hotel", hotelSchema)
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,9 +1,15 @@
-import  express  from "express";
-
-const router=express.Router();
+import express from "express";
+import Hotel from '../models/hotel.js'
+const router = express.Router();
 //CREATE
-router.post('/',(req,res)=>{
-    
+router.post('/', async (req, res) => {
+    const newHotel = new Hotel(req.body)
+    try {
+        const savedHotel = await newHotel
+        res.status(200).json(savedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 //UPDATE
 //DELETE
