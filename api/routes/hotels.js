87a84911bc7fbd4013efb6f1db082968bfdfc290
Johnson <johnsonkarasanyi@gmail.com>
2022-09-27 13:01:06 +0200

    CRUD done

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -21,8 +21,31 @@ router.put('/:id', async (req, res) => {
     }
 })
 //DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //GET
+router.get('/:id', async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //GET ALL
-
+router.get('/', async (req, res) => {
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 export default router
