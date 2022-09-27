fcba2d4bd157e647739e4036fac384fa25e98648
Johnson <johnsonkarasanyi@gmail.com>
2022-09-27 12:49:28 +0200

    Update Hotel api

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,13 +5,21 @@ const router = express.Router();
 router.post('/', async (req, res) => {
     const newHotel = new Hotel(req.body)
     try {
-        const savedHotel = await newHotel
+        const savedHotel = await newHotel.save()
         res.status(200).json(savedHotel)
     } catch (err) {
         res.status(500).json(err)
     }
 })
 //UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set:req.body}, {new :true})
+        res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //DELETE
 //GET
 //GET ALL
