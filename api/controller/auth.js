4cdadf8a01223c871ee1d677d8ef961e155b2a36
Johnson <johnsonkarasanyi@gmail.com>
2022-10-17 12:32:24 +0200

    understanding about authentication

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -29,6 +29,7 @@ export const login = async (req, res, next) => {
             )
         if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"))
         const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT)
+        // Properties which are not needed by user which we have to hide
         const{password,isAdmin,...otherDetails}=user._doc
         res.cookie("access_Token", token,{
             httpOnly:true,
